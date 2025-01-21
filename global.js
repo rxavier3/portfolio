b5ab481e601eef20327d7abcd712311498707545
Ryan Xavier <rxavier@ucsd.edu>
2025-01-21 12:51:47 -0800

    Update global.js

--- global.js
+++ global.js
@@ -29,6 +29,16 @@ for (let p of pages) {
     let title = p.title;
     url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
 
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+    );
+    
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+    nav.append(a);  
   }
