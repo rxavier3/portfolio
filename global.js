dcd043d0d0fd2f4a258a2dedec21ff47657d8173
Ryan Xavier <rxavier@ucsd.edu>
2025-01-21 12:24:53 -0800

    nav

--- global.js
+++ global.js
@@ -1,13 +1,30 @@
 console.log('IT’S ALIVE!');
 
-function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
-}
+// function $$(selector, context = document) {
+//   return Array.from(context.querySelectorAll(selector));
+// }
 
-let navLinks = $$("nav a");
+// let navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
 
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'GitHub/', title: 'GitHub' },
+    { url: 'Resume/', title: 'Resume/CV' }
+  ];
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+  }
