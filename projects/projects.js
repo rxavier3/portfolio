14cd91cf871d586e858bebe6cafe339509be6b5a
Ryan Xavier <rxavier@ucsd.edu>
2025-01-26 12:01:02 -0800

    title

--- projects/projects.js
+++ projects/projects.js
@@ -2,3 +2,13 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+
+
+const projectsCount = projects.length;
+
+const projectsTitle = document.querySelector('.projects-title');
+
+// Update the text content of the projects title with the count
+if (projectsTitle) {
+    projectsTitle.textContent = `(${projectsCount}) Projects`;
+}
