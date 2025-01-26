35af0d124f79f63a80795a9e6bc59f6b5bc3daff
Ryan Xavier <rxavier@ucsd.edu>
2025-01-26 11:49:31 -0800

    made change

--- global.js
+++ global.js
@@ -68,3 +68,29 @@ for (let p of pages) {
       select.value = savedScheme;
   }
 
+export async function fetchJSON(url) {
+  try {
+      const response = await fetch(url);
+      if (!response.ok) {
+          throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+    }
+}
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = ''; // Clear the container first
+
+  projects.forEach(project => {
+      const article = document.createElement('article');
+      article.innerHTML = `
+          <${headingLevel}>${project.title}</${headingLevel}>
+          <img src="${project.image}" alt="${project.title}">
+          <p>${project.description}</p>
+      `;
+      containerElement.appendChild(article);
+  });
+}
+
