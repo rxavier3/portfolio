9be2269142cde5e075b234c65be280528efc8f49
Ryan Xavier <rxavier@ucsd.edu>
2025-02-02 18:56:50 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -13,33 +13,53 @@ if (projectsTitle) {
     projectsTitle.textContent = `${projectsCount} Projects`;
 }
 
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+// let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 // let arc = arcGenerator({
 //     startAngle: 0,
 //     endAngle: 2 * Math.PI,
 //   });
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-let data = [1, 2];
-let total = 0;
+// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+// let data = [1, 2];
+// let total = 0;
 
-for (let d of data) {
-  total += d;
-}
-let angle = 0;
+// for (let d of data) {
+//   total += d;
+// }
+// let angle = 0;
 // let arcData = [];
+
+// for (let d of data) {
+//   let endAngle = angle + (d / total) * 2 * Math.PI;
+//   arcData.push({ startAngle: angle, endAngle });
+//   angle = endAngle;
+// }
+// let arcs = arcData.map((d) => arcGenerator(d));
+// let colors = ['gold', 'purple'];
+// arcs.forEach((arc, idx) => {
+//     d3.select('svg') 
+//       .append('path')
+//       .attr('d', arc)
+//       .attr('fill', colors[idx]) 
+//   });
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+// Data for the pie chart
+let data = [1, 2];
+
+// Use d3.pie() to calculate the start and end angles for each slice
 let sliceGenerator = d3.pie();
 let arcData = sliceGenerator(data);
+
+// Now we use arcGenerator to create the path strings for each slice
 let arcs = arcData.map((d) => arcGenerator(d));
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
-// let arcs = arcData.map((d) => arcGenerator(d));
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+// Use d3.scaleOrdinal to create a color scale
+let colors = d3.scaleOrdinal(d3.schemeTableau10); // Automatically gets a color for each slice
+
+// Select the SVG and append paths for each arc (slice)
 arcs.forEach((arc, idx) => {
-    d3.select('svg') 
-      .append('path')
-      .attr('d', arc)
-      .attr('fill', colors(idx)) 
-  });
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc) // Apply the generated path for the slice
+    .attr('fill', colors(idx));
+});
