54342c72459fab988b6d67cc83828da4e857208a
Ryan Xavier <rxavier@ucsd.edu>
2025-02-02 18:23:16 -0800

    circle

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,4 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
@@ -11,3 +12,11 @@ const projectsTitle = document.querySelector('.projects-title');
 if (projectsTitle) {
     projectsTitle.textContent = `${projectsCount} Projects`;
 }
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arc = arcGenerator({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
