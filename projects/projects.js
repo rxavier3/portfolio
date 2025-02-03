d2b3a2e0a562269f526a536df9e43198344b0ea0
Ryan Xavier <rxavier@ucsd.edu>
2025-02-02 17:52:14 -0800

    Revert "Update projects.js"
    This reverts commit 91ed9b61b1d61cde65bea34ef5bd23a312145d9b.

--- projects/projects.js
+++ projects/projects.js
@@ -21,38 +21,25 @@ let arc = arcGenerator({
   });
   d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-// let data = [1, 2];
-// let total = 0;
+let data = [1, 2];
+let total = 0;
 
-// for (let d of data) {
-//     total += d;
-// }
-// let angle = 0;
-// let arcData = [];
+for (let d of data) {
+    total += d;
+}
+let angle = 0;
+let arcData = [];
 
-// for (let d of data) {
-//   let endAngle = angle + (d / total) * 2 * Math.PI;
-//   arcData.push({ startAngle: angle, endAngle });
-//   angle = endAngle;
-// }
-// let arcs = arcData.map((d) => arcGenerator(d));
-
-// let colors = ['gold', 'purple'];
-// arcs.forEach((arc, i) => {
-//   d3.select('svg')
-//     .append("path")
-//     .attr("d", arc)
-//     .attr('fill', colors[i]); 
-// });
-let data = [1, 2, 3, 4, 5, 5];
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
-
-let sliceGenerator = d3.pie();
-let arcData = sliceGenerator(data);
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
 let arcs = arcData.map((d) => arcGenerator(d));
+const svg = d3.select("svg");
+let colors = ['gold', 'purple'];
 arcs.forEach((arc, i) => {
-   d3.select('svg')
-     .append("path")
-     .attr("d", arc)
-    .attr('fill', colors(i)); 
+  svg.append("path")
+    .attr("d", arc)
+    .attr('fill', colors[i]); 
 });
